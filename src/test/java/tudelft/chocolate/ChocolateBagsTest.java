5ce1d55ad38bc5e41d1f4c9d6418eb05d8dcef09
renida <50323160+renidalumi@users.noreply.github.com>
2021-12-31 10:24:31 +0200

    Update ChocolateBagsTest.java

--- src/test/java/tudelft/chocolate/ChocolateBagsTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsTest.java
@@ -22,7 +22,7 @@ public class ChocolateBagsTest {
 
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
     @CsvSource({
-        "0,3,17,-1", "1,3,17,-1", "2,3,17,2", "3,3,17,2",
+        "0,3,17,-1", "1,3,17,-1", "3,6,27,12", "3,3,17,2",
         "0,3,12,-1", "1,3,12,-1", "2,3,12,2", "3,3,12,2"})
     public void bigAndSmallBars(int small, int big, int total, int expectedResult) {
         int result = new ChocolateBags().calculate(small, big, total);
